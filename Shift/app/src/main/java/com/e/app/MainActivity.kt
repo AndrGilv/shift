cbda939b6d78b2084eb83f5c3be2f2c606479b45
Gilev Andrey <andrgilv@gmail.com>
2020-02-05 17:00:40 +0700

    qwert

--- Shift/app/src/main/java/com/e/app/MainActivity.kt
+++ Shift/app/src/main/java/com/e/app/MainActivity.kt
@@ -10,5 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    Button buttonToJim =
+
 }
--- Shift/app/src/main/java/com/e/app/sportServices.kt
+++ Shift/app/src/main/java/com/e/app/sportServices.kt
@@ -1,12 +1,44 @@
 package com.e.app
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
 class sportServices : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sport_services)
+
+
+        val image : ImageView = findViewById(R.id.serviceImage)
+        val buttonToJim : Button = findViewById(R.id.buttonToJimService)
+        val buttonToCardio : Button = findViewById(R.id.buttonToCardio)
+        val buttonToYoga : Button = findViewById(R.id.buttonToYoga)
+        val buttonToBrus : Button = findViewById(R.id.buttonToBrusya)
+        val buttonToMMA : Button = findViewById(R.id.buttonToMMA)
+
+        buttonToJim.setOnClickListener(this::clickEvent)
+        buttonToCardio.setOnClickListener(this::clickEvent)
+        buttonToYoga.setOnClickListener(this::clickEvent)
+        buttonToBrus.setOnClickListener(this::clickEvent)
+        buttonToMMA.setOnClickListener(this::clickEvent)
     }
+
+    private fun clickEvent(view: View){
+        val myIntent = Intent(this, calendar::class.java)
+        //myIntent.putExtra()
+
+    }
+
+
+
+
+    val myIntent = Intent(this, calendar::class.java)
+
+
 }
